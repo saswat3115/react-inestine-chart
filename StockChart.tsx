0b16edd9705b5da9a694b6699a9d4a0f4f871bf3
saswat3115 <saswat3115@gmail.com>
2022-02-07 16:38:37 +0530

    1 file modified

--- StockChart.tsx
+++ StockChart.tsx
@@ -8,18 +8,30 @@ function getRandomArbitrary(min, max) {
 
 function getData(days) {
   var data = [];
+  var x = [];
+  const today = new Date();
+  today.setDate(today.getDate() - days); // 60 days before date
+
   for (let i = 0; i < days; i++) {
     data.push(getRandomArbitrary(500, 5000));
+
+    // today.setDate(today.getDate() + i);
+    x.push(i + 1);
   }
-  return data;
+  return {
+    x,
+    data,
+  };
 }
+
+console.log(getData(60).x);
 
 const options = {
   chart: {
     zoomType: 'x',
     height: '250px',
     scrollablePlotArea: {
-      minWidth: 700,
+      minWidth: 2000,
       scrollPositionX: 1,
     },
   },
@@ -31,11 +43,17 @@ const options = {
     text: '',
   },
   xAxis: {
-    // categories: [], // ['01', '02', '03', '04', '05', '06', '07', '08'],
+    type: '',
+    categories: getData(60).x, // ['01', '02', '03', '04', '05', '06', '07', '08'],
     title: {
       text: '',
     },
-    tickInterval: 1,
+    tickPixelInterval: 50,
+    labels: {
+      step: 1,
+      distance: 100,
+    },
+    // tickInterval: 1,
     gridLineWidth: 0,
     lineColor: 'transparent',
   },
@@ -47,6 +65,9 @@ const options = {
     gridLineWidth: 0,
   },
   plotOptions: {
+    // column: {
+    //   pointWidth: 80,
+    // },
     series: {
       color: '#E54F6D',
     },
@@ -68,8 +89,9 @@ const options = {
   },
   series: [
     {
-      data: getData(60), // [2200, 3000, 1800, 3600, 4000, 3800, 3900, 2700],
+      data: getData(60).data, // [2200, 3000, 1800, 3600, 4000, 3800, 3900, 2700],
       type: 'area',
+      // pointInterval: 24 * 3600 * 1000,
       marker: {
         fillColor: '#FFFFFF',
         radius: 3,
