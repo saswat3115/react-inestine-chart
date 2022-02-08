1ad4c2f4c1e9313b1493354446518562c1b2f20a
saswat3115 <saswat3115@gmail.com>
2022-02-09 02:03:34 +0530

    2 files modified

--- Intestine.tsx
+++ Intestine.tsx
@@ -3,48 +3,77 @@ import React, { Component, useMemo } from 'react';
 const H = 36;
 const W = 36;
 
+const getArcPath = (x, y, rx, ry, d) => {
+  return `M${x} ${y} a ${rx} ${ry} 0 0 1 0 ${d} a ${rx} ${ry} 0 0 1 0 -${d}`;
+};
+
 const Intestine = (props) => {
   const { width, borderWidth, gap, data } = props;
   const start = {
-    x: W / 2,
-    y: H / Math.PI / 2,
+    x: 18,
+    y: 2.0845,
   };
   return (
     <svg viewBox={`0 0 ${H} ${W}`} className="circular-chart">
-      {Object.keys(data).map((item) => {
+      {Object.keys(data).map((item, index) => {
         const fillPercentage = (data[item].actual / data[item].estimated) * 100;
+
+        const maxArcPercentage = (data[item].maxAngle / 360) * 100;
+        const fillArcPercentage = (maxArcPercentage * fillPercentage) / 100;
+
         return (
           <g key={item}>
             <path
               className="circle-bg"
               strokeLinecap="round"
-              d="M18 2.0845
-              a 15.9155 15.9155 0 0 1 0 31.831
-              a 15.9155 15.9155 0 0 1 0 -31.831"
+              // d="M18 2.0845
+              // a 15.9155 15.9155 0 0 1 0 31.831
+              // a 15.9155 15.9155 0 0 1 0 -31.831"
+              d={getArcPath(
+                start.x,
+                start.y + index * 4,
+                15.9155 - index * 4,
+                15.9155 - index * 4,
+                (15.9155 - index * 4) * 2
+              )}
               fill="none"
-              strokeDasharray={`75 100`}
+              strokeDasharray={`${maxArcPercentage} 100`}
               strokeWidth={width}
               stroke="red"
             />
             <path
               className="circle-white"
               strokeLinecap="round"
-              d="M18 2.0845
-              a 15.9155 15.9155 0 0 1 0 31.831
-              a 15.9155 15.9155 0 0 1 0 -31.831"
+              // d="M18 2.0845
+              // a 15.9155 15.9155 0 0 1 0 31.831
+              // a 15.9155 15.9155 0 0 1 0 -31.831"
+              d={getArcPath(
+                start.x,
+                start.y + index * 4,
+                15.9155 - index * 4,
+                15.9155 - index * 4,
+                (15.9155 - index * 4) * 2
+              )}
               fill="none"
-              strokeDasharray={`75 100`}
+              strokeDasharray={`${maxArcPercentage} 100`}
               strokeWidth={width - borderWidth}
               stroke="white"
             />
             <path
               className="circle"
               strokeLinecap="round"
-              d="M18 2.0845
-              a 15.9155 15.9155 0 0 1 0 31.831
-              a 15.9155 15.9155 0 0 1 0 -31.831"
+              // d="M18 2.0845
+              // a 15.9155 15.9155 0 0 1 0 31.831
+              // a 15.9155 15.9155 0 0 1 0 -31.831"
+              d={getArcPath(
+                start.x,
+                start.y + index * 4,
+                15.9155 - index * 4,
+                15.9155 - index * 4,
+                (15.9155 - index * 4) * 2
+              )}
               fill="none"
-              strokeDasharray={`${fillPercentage} 100`}
+              strokeDasharray={`${fillArcPercentage} 100`}
               strokeWidth={width - borderWidth}
               stroke="yellow"
             />
--- index.tsx
+++ index.tsx
@@ -9,28 +9,28 @@ import Intestine from './Intestine';
 const data = {
   EXPENSE: {
     estimated: 15000,
-    actual: 9000,
+    actual: 10000,
     color: '#FF4259',
     maxAngle: 270,
   },
-  // INCOME: {
-  //   estimated: 150000,
-  //   actual: 150000,
-  //   color: '#009C5B',
-  //   maxAngle: 180,
-  // },
-  // SAVING: {
-  //   estimated: 100000,
-  //   actual: 80000,
-  //   color: '#F8C630',
-  //   maxAngle: 120,
-  // },
-  // INVEST: {
-  //   estimated: 25000,
-  //   actual: 22000,
-  //   color: '#45A6FF',
-  //   maxAngle: 90,
-  // },
+  INCOME: {
+    estimated: 150000,
+    actual: 150000,
+    color: '#009C5B',
+    maxAngle: 180,
+  },
+  SAVING: {
+    estimated: 100000,
+    actual: 80000,
+    color: '#F8C630',
+    maxAngle: 120,
+  },
+  INVEST: {
+    estimated: 25000,
+    actual: 22000,
+    color: '#45A6FF',
+    maxAngle: 90,
+  },
 };
 
 interface AppProps {}
