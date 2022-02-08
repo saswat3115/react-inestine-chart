fee4305f24d9ea52783b7e27450b8a059136e730
saswat3115 <saswat3115@gmail.com>
2022-02-09 01:36:31 +0530

    3 files modified

--- Intestine.tsx
+++ Intestine.tsx
@@ -1,3 +1,5 @@
+import React, { Component, useMemo } from 'react';
+
 const H = 36;
 const W = 36;
 
@@ -10,14 +12,47 @@ const Intestine = (props) => {
   return (
     <svg viewBox={`0 0 ${H} ${W}`} className="circular-chart">
       {Object.keys(data).map((item) => {
+        const fillPercentage = (data[item].actual / data[item].estimated) * 100;
         return (
           <g key={item}>
-            <path className="circle-bg" />
-            <path className="circle-white" />
-            <path className="circle" />
+            <path
+              className="circle-bg"
+              strokeLinecap="round"
+              d="M18 2.0845
+              a 15.9155 15.9155 0 0 1 0 31.831
+              a 15.9155 15.9155 0 0 1 0 -31.831"
+              fill="none"
+              strokeDasharray={`75 100`}
+              strokeWidth={width}
+              stroke="red"
+            />
+            <path
+              className="circle-white"
+              strokeLinecap="round"
+              d="M18 2.0845
+              a 15.9155 15.9155 0 0 1 0 31.831
+              a 15.9155 15.9155 0 0 1 0 -31.831"
+              fill="none"
+              strokeDasharray={`75 100`}
+              strokeWidth={width - borderWidth}
+              stroke="white"
+            />
+            <path
+              className="circle"
+              strokeLinecap="round"
+              d="M18 2.0845
+              a 15.9155 15.9155 0 0 1 0 31.831
+              a 15.9155 15.9155 0 0 1 0 -31.831"
+              fill="none"
+              strokeDasharray={`${fillPercentage} 100`}
+              strokeWidth={width - borderWidth}
+              stroke="yellow"
+            />
           </g>
         );
       })}
     </svg>
   );
 };
+
+export default Intestine;
--- index.tsx
+++ index.tsx
@@ -4,6 +4,7 @@ import Hello from './Hello';
 import './style.css';
 import Chart from './Chart';
 import StockChart from './StockChart';
+import Intestine from './Intestine';
 
 const data = {
   EXPENSE: {
@@ -12,24 +13,24 @@ const data = {
     color: '#FF4259',
     maxAngle: 270,
   },
-  INCOME: {
-    estimated: 150000,
-    actual: 150000,
-    color: '#009C5B',
-    maxAngle: 180,
-  },
-  SAVING: {
-    estimated: 100000,
-    actual: 80000,
-    color: '#F8C630',
-    maxAngle: 120,
-  },
-  INVEST: {
-    estimated: 25000,
-    actual: 22000,
-    color: '#45A6FF',
-    maxAngle: 90,
-  },
+  // INCOME: {
+  //   estimated: 150000,
+  //   actual: 150000,
+  //   color: '#009C5B',
+  //   maxAngle: 180,
+  // },
+  // SAVING: {
+  //   estimated: 100000,
+  //   actual: 80000,
+  //   color: '#F8C630',
+  //   maxAngle: 120,
+  // },
+  // INVEST: {
+  //   estimated: 25000,
+  //   actual: 22000,
+  //   color: '#45A6FF',
+  //   maxAngle: 90,
+  // },
 };
 
 interface AppProps {}
@@ -48,15 +49,16 @@ class App extends Component<AppProps, AppState> {
   render() {
     return (
       <div style={{ padding: '20px' }}>
-        <Chart
+        {/* <Chart
           data={data}
           radius={200}
           active=""
           width={24}
           borderWidth={4}
           gap={35}
-        />
+        /> */}
         {/* <StockChart /> */}
+        <Intestine data={data} width={3} borderWidth={1} gap={2} />
       </div>
     );
   }
